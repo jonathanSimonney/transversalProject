776796f2a829ff704228098822e432f0d7e08e24
Jonathan SIMONNEY <jonathan.simonney@supinternet.fr>
2017-05-05 13:00:36 +0200

    changed list of contact for pro (suppressed other pro), and prepared in config the route for suppression of articles.

--- Model/ProManager.php
+++ Model/ProManager.php
@@ -19,7 +19,7 @@ class ProManager extends UserManager
     public function getContact()
     {
         $ret = $this->DBManager->findAll('SELECT `id`, `pseudo`, `location`, `type`
-        FROM users WHERE (type != \'victime\' AND id != '.$_SESSION['currentUser']['data']['id'].') OR psy_id = '.$_SESSION['currentUser']['data']['id'].' OR lawyer_id = '.$_SESSION['currentUser']['data']['id']);
+        FROM users WHERE psy_id = '.$_SESSION['currentUser']['data']['id'].' OR lawyer_id = '.$_SESSION['currentUser']['data']['id']);
 
         return $this->makeInferiorKeyIndex($ret, 'pseudo');
     }
